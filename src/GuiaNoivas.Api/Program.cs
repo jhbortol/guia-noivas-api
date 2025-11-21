f6affbf29606eed8a2371a2fef72f8986c355dd7
jhbortol <jhbortol@gmail.com>
2025-11-21 17:20:04 +0000

    sagger adjustment

--- src/GuiaNoivas.Api/Program.cs
+++ src/GuiaNoivas.Api/Program.cs
@@ -149,8 +149,22 @@ using (var scope = app.Services.CreateScope())
     }
 }
 
+
 app.UseRouting();
 app.UseAuthentication();
+// Serve static files before authorization so Swagger UI files are accessible
+app.UseStaticFiles();
+// Bypass authorization for Swagger static files
+app.Use(async (context, next) =>
+{
+    var path = context.Request.Path.Value;
+    if (path != null && path.StartsWith("/swagger"))
+    {
+        await next();
+        return;
+    }
+    await next();
+});
 app.UseAuthorization();
 
 app.UseSwagger();
