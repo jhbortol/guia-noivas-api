40ad445414e3b4fa8df8ec3be265f30b857bd8b0
jhbortol <jhbortol@gmail.com>
2025-11-21 12:51:48 +0000

    Add EF migration for ContatoSubmission and InstitucionalContent

--- src/GuiaNoivas.Api/Migrations/20251121125141_AddContatoAndInstitucional.Designer.cs
+++ src/GuiaNoivas.Api/Migrations/20251121125141_AddContatoAndInstitucional.Designer.cs
@@ -4,6 +4,7 @@ using GuiaNoivas.Api.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace GuiaNoivas.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251121125141_AddContatoAndInstitucional")]
+    partial class AddContatoAndInstitucional
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
